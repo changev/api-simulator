a4f9c822be8ccf31a2654cc4cbce3f7bb1dfbe40
Wei <Alan.Wei@emc.com>
2018-03-08 16:22:35 +0800

    add login and get current status logic

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // server.js
 const path = require('path')
 const http = require('http')
+const md5 = require('md5')
 const jsonServer = require('./src/server')
 const app = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
@@ -31,15 +32,31 @@ app.use(function(req, res, next) {
 
 // customized routes
 // login
-const username = 'admin'
-const password = 'admin'
 app.post('/login', (req, res) => {
   var params = req.body;
-  if (params && params.username === username && params.password === password){
-     res.status(200).send({'token': '12345'});
-  } else {
-     res.status(401).send({'msg': 'Wrong username or password'});
+  var db = require(path.join(__dirname, 'db.json'));
+  var users = db.users;
+  for (var i = 0; i < users.length; i++) {
+    if (params && params.username === user[i].username && params.password === users[i].password){
+       res.status(200).send({'token': md5(user[i].username+user[i].password)});
+       return;
+     }
   }
+  res.status(401).send({'msg': 'Wrong username or password'});
+})
+
+app.get('/users/current', (req, res) => {
+  var token = req.headers.token;
+  var db = require(path.join(__dirname, 'db.json'));
+  var users = db.users;
+  for (var i = 0; i < users.length; i++) {
+    var thisToken = md5(users[i].username + users[i].password);
+    if(thisToken === token){
+      res.status(200).send(users[i]);
+      return;
+    }
+  }
+  res.status(200).send({});
 })
 
 var db = require(path.join(__dirname, 'db.json'))
@@ -54,7 +71,7 @@ app.use(rewriter)
 app.use(router)
 
 var httpServer = http.createServer(app)
-httpServer.listen(9999, () => {
+httpServer.listen(9998, () => {
   console.log('JSON Server is running')
 })
 
